dbff0c038779fcf823dee010e9d6b9175de72aef
clodaghsmurphy <clodagh9@yahoo.ie>
2023-04-23 22:29:19 +0200

    css not working

--- html/index.php
+++ html/index.php
@@ -1,4 +1,19 @@
-<?php
-header("Content-type: text/html");
-echo "<h1>Hello, World!</h1>";
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>test</title>
+	<link rel="stylesheet" href="./styles.css" type="text/css" >
+</head>
+<body>
+	<header class="nav-bar"> 
+		<ul class="nav-list">
+			<li class="nav-item"></li>
+			<li class="nav-item"></li>
+			<li class="nav-item"></li>
+	</header>
+	
+</body>
+</html>
